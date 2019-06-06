acb0c5a6a0da4b0f5874daf5881126341d203249
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-06-06 20:59:35 +0200

    Test regenerator provider with preset-env

---
+++ packages/babel-polyfill-provider-regenerator/test/fixtures/usage-global/preset-env-unsupported/output.mjs
@@ -0,0 +1,17 @@
+import "regenerator-runtime/runtime";
+
+var _marked =
+/*#__PURE__*/
+regeneratorRuntime.mark(f);
+
+function f() {
+  return regeneratorRuntime.wrap(function f$(_context) {
+    while (1) {
+      switch (_context.prev = _context.next) {
+        case 0:
+        case "end":
+          return _context.stop();
+      }
+    }
+  }, _marked);
+}
