e3998cb1e9d1b5b8c3cbbd0e1495a8fe70ed3d72
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-06-13 02:11:59 +0200

    Add usage-pure to corejs3

---
+++ packages/babel-polyfill-provider-corejs3/test/fixtures/usage-pure/modules-loose/output.js
@@ -0,0 +1,30 @@
+"use strict";
+
+var _forEachInstanceProperty = require("@babel/runtime-corejs3/core-js-stable/instance/for-each");
+
+var _Object$keys = require("@babel/runtime-corejs3/core-js-stable/object/keys");
+
+exports.__esModule = true;
+var _exportNames = {
+  exp: true
+};
+exports.exp = void 0;
+
+var _bar = _interopRequireDefault(require("bar"));
+
+var _fuz = require("fuz");
+
+var _mod = require("mod");
+
+_forEachInstanceProperty(_context = _Object$keys(_mod)).call(_context, function (key) {
+  if (key === "default" || key === "__esModule") return;
+  if (Object.prototype.hasOwnProperty.call(_exportNames, key)) return;
+  exports[key] = _mod[key];
+});
+
+var _context;
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const exp = _bar.default + _fuz.baz;
+exports.exp = exp;
