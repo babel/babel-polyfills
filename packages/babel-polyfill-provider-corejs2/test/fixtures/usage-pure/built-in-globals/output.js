4de4c4c35b6bd06281f5efe1477913e498d0a55f
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-06-22 00:34:11 +0200

    More tests for corejs2 usage-pure

---
+++ packages/babel-polyfill-provider-corejs2/test/fixtures/usage-pure/built-in-globals/output.js
@@ -0,0 +1,43 @@
+var _Map = require("@babel/runtime-corejs2/core-js/map");
+
+var _Promise = require("@babel/runtime-corejs2/core-js/promise");
+
+var _Set = require("@babel/runtime-corejs2/core-js/set");
+
+var _Symbol = require("@babel/runtime-corejs2/core-js/symbol");
+
+var _WeakMap = require("@babel/runtime-corejs2/core-js/weak-map");
+
+var _WeakSet = require("@babel/runtime-corejs2/core-js/weak-set");
+
+AggregateError;
+_Map;
+Observable;
+_Promise;
+_Set;
+_Symbol;
+URL;
+URLSearchParams;
+_WeakMap;
+_WeakSet;
+clearImmediate;
+compositeKey;
+compositeSymbol;
+globalThis;
+parseFloat;
+parseInt;
+queueMicrotask;
+setImmediate;
+setInterval;
+setTimeout;
+Array;
+Boolean;
+Date;
+Infinity;
+JSON;
+NaN;
+Number;
+Object;
+clearInterval;
+clearTimeout;
+something;
---
+++ packages/babel-polyfill-provider-corejs2/test/fixtures/usage-pure/built-in-static-methods/output.js
@@ -0,0 +1,179 @@
+var _Array$from = require("@babel/runtime-corejs2/core-js/array/from");
+
+var _Array$isArray = require("@babel/runtime-corejs2/core-js/array/is-array");
+
+var _Array$of = require("@babel/runtime-corejs2/core-js/array/of");
+
+var _Date$now = require("@babel/runtime-corejs2/core-js/date/now");
+
+var _Number$EPSILON = require("@babel/runtime-corejs2/core-js/number/epsilon");
+
+var _Number$MAX_SAFE_INTEGER = require("@babel/runtime-corejs2/core-js/number/max-safe-integer");
+
+var _Number$MIN_SAFE_INTEGER = require("@babel/runtime-corejs2/core-js/number/min-safe-integer");
+
+var _Number$isFinite = require("@babel/runtime-corejs2/core-js/number/is-finite");
+
+var _Number$isInteger = require("@babel/runtime-corejs2/core-js/number/is-integer");
+
+var _Number$isNaN = require("@babel/runtime-corejs2/core-js/number/is-nan");
+
+var _Number$isSafeInteger = require("@babel/runtime-corejs2/core-js/number/is-safe-integer");
+
+var _Number$parseFloat = require("@babel/runtime-corejs2/core-js/number/parse-float");
+
+var _Number$parseInt = require("@babel/runtime-corejs2/core-js/number/parse-int");
+
+var _Object$assign = require("@babel/runtime-corejs2/core-js/object/assign");
+
+var _Object$create = require("@babel/runtime-corejs2/core-js/object/create");
+
+var _Object$defineProperties = require("@babel/runtime-corejs2/core-js/object/define-properties");
+
+var _Object$defineProperty = require("@babel/runtime-corejs2/core-js/object/define-property");
+
+var _Object$entries = require("@babel/runtime-corejs2/core-js/object/entries");
+
+var _Object$freeze = require("@babel/runtime-corejs2/core-js/object/freeze");
+
+var _Object$getOwnPropertyDescriptors = require("@babel/runtime-corejs2/core-js/object/get-own-property-descriptors");
+
+var _Object$getOwnPropertySymbols = require("@babel/runtime-corejs2/core-js/object/get-own-property-symbols");
+
+var _Object$isExtensible = require("@babel/runtime-corejs2/core-js/object/is-extensible");
+
+var _Object$isFrozen = require("@babel/runtime-corejs2/core-js/object/is-frozen");
+
+var _Object$isSealed = require("@babel/runtime-corejs2/core-js/object/is-sealed");
+
+var _Object$is = require("@babel/runtime-corejs2/core-js/object/is");
+
+var _Object$keys = require("@babel/runtime-corejs2/core-js/object/keys");
+
+var _Object$preventExtensions = require("@babel/runtime-corejs2/core-js/object/prevent-extensions");
+
+var _Object$seal = require("@babel/runtime-corejs2/core-js/object/seal");
+
+var _Object$setPrototypeOf = require("@babel/runtime-corejs2/core-js/object/set-prototype-of");
+
+var _Object$values = require("@babel/runtime-corejs2/core-js/object/values");
+
+var _Reflect$apply = require("@babel/runtime-corejs2/core-js/reflect/apply");
+
+var _Reflect$construct = require("@babel/runtime-corejs2/core-js/reflect/construct");
+
+var _Reflect$defineProperty = require("@babel/runtime-corejs2/core-js/reflect/define-property");
+
+var _Reflect$deleteProperty = require("@babel/runtime-corejs2/core-js/reflect/delete-property");
+
+var _Reflect$getOwnPropertyDescriptor = require("@babel/runtime-corejs2/core-js/reflect/get-own-property-descriptor");
+
+var _Reflect$getPrototypeOf = require("@babel/runtime-corejs2/core-js/reflect/get-prototype-of");
+
+var _Reflect$get = require("@babel/runtime-corejs2/core-js/reflect/get");
+
+var _Reflect$has = require("@babel/runtime-corejs2/core-js/reflect/has");
+
+var _Reflect$isExtensible = require("@babel/runtime-corejs2/core-js/reflect/is-extensible");
+
+var _Reflect$ownKeys = require("@babel/runtime-corejs2/core-js/reflect/own-keys");
+
+var _Reflect$preventExtensions = require("@babel/runtime-corejs2/core-js/reflect/prevent-extensions");
+
+var _Reflect$set = require("@babel/runtime-corejs2/core-js/reflect/set");
+
+var _Reflect$setPrototypeOf = require("@babel/runtime-corejs2/core-js/reflect/set-prototype-of");
+
+var _String$fromCodePoint = require("@babel/runtime-corejs2/core-js/string/from-code-point");
+
+var _String$raw = require("@babel/runtime-corejs2/core-js/string/raw");
+
+var _Symbol = require("@babel/runtime-corejs2/core-js/symbol");
+
+var _Symbol$iterator = require("@babel/runtime-corejs2/core-js/symbol/iterator");
+
+var _Symbol$match = require("@babel/runtime-corejs2/core-js/symbol/match");
+
+_Array$from;
+_Array$isArray;
+_Array$of;
+_Date$now;
+JSON.stringify;
+_Number$EPSILON;
+_Number$MAX_SAFE_INTEGER;
+_Number$MIN_SAFE_INTEGER;
+Number.fromString;
+_Number$isFinite;
+_Number$isInteger;
+_Number$isNaN;
+_Number$isSafeInteger;
+_Number$parseFloat;
+_Number$parseInt;
+_Object$assign;
+_Object$create;
+_Object$defineProperties;
+_Object$defineProperty;
+_Object$entries;
+_Object$freeze;
+Object.fromEntries;
+Object.getOwnPropertyDescriptor;
+_Object$getOwnPropertyDescriptors;
+Object.getOwnPropertyNames;
+_Object$getOwnPropertySymbols;
+Object.getPrototypeOf;
+_Object$isExtensible;
+_Object$isFrozen;
+_Object$isSealed;
+_Object$is;
+_Object$keys;
+_Object$preventExtensions;
+_Object$seal;
+_Object$setPrototypeOf;
+_Object$values;
+_Reflect$apply;
+_Reflect$construct;
+Reflect.defineMetadata;
+_Reflect$defineProperty;
+Reflect.deleteMetadata;
+_Reflect$deleteProperty;
+Reflect.getMetadata;
+Reflect.getMetadataKeys;
+Reflect.getOwnMetadata;
+Reflect.getOwnMetadataKeys;
+_Reflect$getOwnPropertyDescriptor;
+_Reflect$getPrototypeOf;
+_Reflect$get;
+_Reflect$has;
+Reflect.hasMetadata;
+Reflect.hasOwnMetadata;
+_Reflect$isExtensible;
+Reflect.metadata;
+_Reflect$ownKeys;
+_Reflect$preventExtensions;
+_Reflect$set;
+_Reflect$setPrototypeOf;
+_String$fromCodePoint;
+_String$raw;
+_Symbol.asyncIterator;
+_Symbol.dispose;
+_Symbol.for;
+_Symbol.hasInstance;
+_Symbol.isConcatSpreadable;
+_Symbol$iterator;
+_Symbol.keyFor;
+_Symbol$match;
+_Symbol.observable;
+_Symbol.patternMatch;
+_Symbol.replace;
+_Symbol.search;
+_Symbol.species;
+_Symbol.split;
+_Symbol.toPrimitive;
+_Symbol.toStringTag;
+_Symbol.unscopables;
+Foo.bar;
+Array.something;
+Date.something;
+JSON.parse;
+Math.pow;
+_Symbol.something;
