91fcc04bdeea35462ab9d23f597dccbf54438250
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-06-23 23:06:50 +0200

    Backward compat corejs2 with preset-env

--- packages/babel-polyfill-provider-corejs2/src/built-in-definitions.js
+++ packages/babel-polyfill-provider-corejs2/src/built-in-definitions.js
@@ -55,7 +55,7 @@ export const BuiltIns: ObjectMap<PolyfillDescriptor<CoreJS2Meta>> = {
   Promise: pureAndGlobal("promise", PromiseDependencies),
   RegExp: globalOnly(["es6.regexp.constructor"]),
   Set: pureAndGlobal("set", ["es6.set", ...CommonIterators]),
-  Symbol: pureAndGlobal("symbol", ["es6.symbol"]),
+  Symbol: pureAndGlobal("symbol", ["es6.symbol", "es7.symbol.async-iterator"]),
   Uint8Array: globalOnly(["es6.typed.uint8-array"]),
   Uint8ClampedArray: globalOnly(["es6.typed.uint8-clamped-array"]),
   Uint16Array: globalOnly(["es6.typed.uint16-array"]),
--- packages/babel-polyfill-provider-corejs2/test/fixtures/usage-global/symbol-iterator/output.mjs
+++ packages/babel-polyfill-provider-corejs2/test/fixtures/usage-global/symbol-iterator/output.mjs
@@ -3,4 +3,5 @@ import "core-js/modules/es6.object.to-string";
 import "core-js/modules/es6.array.iterator";
 import "core-js/modules/web.dom.iterable";
 import "core-js/modules/es6.symbol";
+import "core-js/modules/es7.symbol.async-iterator";
 arr[Symbol.iterator]();
