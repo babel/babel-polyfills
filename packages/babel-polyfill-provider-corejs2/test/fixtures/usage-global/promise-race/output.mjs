a5a8e6aaaf5d2a7a6574d543c2345efe710abaa9
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2019-06-23 22:47:20 +0200

    corejs2 don't load unnecessary symbol polyfill

--- packages/babel-polyfill-provider-corejs2/test/fixtures/usage-global/promise-race/output.mjs
+++ packages/babel-polyfill-provider-corejs2/test/fixtures/usage-global/promise-race/output.mjs
@@ -1,6 +1,5 @@
 import "core-js/modules/es6.object.to-string";
 import "core-js/modules/es6.promise";
-import "core-js/modules/es6.symbol";
 import "core-js/modules/es6.string.iterator";
 import "core-js/modules/es6.array.iterator";
 import "core-js/modules/web.dom.iterable";
