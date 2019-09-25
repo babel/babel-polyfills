f4e9dfec194995cfb97a66fedbc3f62f7824505c
Thiago Arrais <thiago.arrais@gmail.com>
2019-09-25 13:47:14 -0300

    Check for computed instance properties (test only)
    Ensures that https://github.com/babel/babel/pull/10207 is resolved

---
+++ packages/babel-polyfill-provider-corejs3/test/fixtures/usage-pure/instance-computed/input.js
@@ -0,0 +1,4 @@
+var filter = 'foo';
+var bar = { foo: function() { return 'foo'; }}
+
+bar[filter]();
---
+++ packages/babel-polyfill-provider-corejs3/test/fixtures/usage-pure/instance-computed/output.js
@@ -0,0 +1,7 @@
+var filter = 'foo';
+var bar = {
+  foo: function () {
+    return 'foo';
+  }
+};
+bar[filter]();
