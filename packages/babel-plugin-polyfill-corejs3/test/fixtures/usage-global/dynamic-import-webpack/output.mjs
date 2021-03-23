e2891aede7eae2e2ae7b620b524fa78599eeb5b5
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-03-23 16:49:47 +0100

    [corejs3] Inject usage-global imports in the correct order (#78)

--- packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-global/dynamic-import-webpack/output.mjs
+++ packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-global/dynamic-import-webpack/output.mjs
@@ -1,7 +1,7 @@
-import "core-js/modules/es.promise.js";
-import "core-js/modules/es.object.to-string.js";
-import "core-js/modules/es.string.iterator.js";
 import "core-js/modules/es.array.iterator.js";
+import "core-js/modules/es.object.to-string.js";
+import "core-js/modules/es.promise.js";
+import "core-js/modules/es.string.iterator.js";
 import "core-js/modules/web.dom-collections.iterator.js";
 // https://github.com/babel/babel/issues/9872
 import("foo");
