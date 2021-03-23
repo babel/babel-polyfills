3fe8c137c485c34f71ecbdc1542307f9de913b35
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2021-03-23 15:12:19 +0100

    [helper-polyfill-provider] Fully resolve absolute imports

--- packages/babel-helper-define-polyfill-provider/test/fixtures/absoluteImports/string/main.out.js
+++ packages/babel-helper-define-polyfill-provider/test/fixtures/absoluteImports/string/main.out.js
@@ -1,4 +1,4 @@
-import "<CWD>/fixtures/absoluteImports/string/nested/node_modules/polyfill-a";
-import "<CWD>/fixtures/absoluteImports/string/node_modules/polyfill-b";
+import "<CWD>/fixtures/absoluteImports/string/nested/node_modules/polyfill-a/index.js";
+import "<CWD>/fixtures/absoluteImports/string/node_modules/polyfill-b/index.js";
 a;
 b;
