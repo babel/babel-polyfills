995c592eef28432379574b4e326ced566fae7df0
Huáng Jùnliàng <jlhwung@gmail.com>
2021-08-24 17:48:40 -0400

    update test fixtures

--- packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-global/all-proposals-chrome-90-corejs-3.9/output.mjs
+++ packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-global/all-proposals-chrome-90-corejs-3.9/output.mjs
@@ -1,4 +1,6 @@
 import "core-js/modules/esnext.array.find-last.js";
+import "core-js/modules/esnext.typed-array.find-last.js";
 import "core-js/modules/esnext.array.find-last-index.js";
+import "core-js/modules/esnext.typed-array.find-last-index.js";
 [].findLast(fn);
 [].findLastIndex(fn);
--- packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-global/array-flat/output.mjs
+++ packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-global/array-flat/output.mjs
@@ -1,4 +1,5 @@
 import "core-js/modules/es.array.flat.js";
+import "core-js/modules/es.array.unscopables.flat.js";
 import "core-js/modules/es.array.flat-map.js";
 import "core-js/modules/es.array.unscopables.flat-map.js";
 [].flat();
