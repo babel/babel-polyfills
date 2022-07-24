5a4d4a665fb0ec840a310f12c1c1313cb17c40b0
Nicolò Ribaudo <nicolo.ribaudo@gmail.com>
2022-07-24 08:40:44 +0200

    Update compat-table

--- scripts/download-compat-table.sh
+++ scripts/download-compat-table.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-COMPAT_TABLE_COMMIT=c52fd057b5685d3e8fdc586763e5d59a20db5c11
+COMPAT_TABLE_COMMIT=9742598c5883018d68885e766840b4c5d7efab8f
 GIT_HEAD=build/compat-table/.git/HEAD
 
 if [ -d "build/compat-table" ]; then
