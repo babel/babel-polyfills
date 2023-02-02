8dd15b4575078f4fec17d8be0e805c077e11c797
liuxingbaoyu <30521560+liuxingbaoyu@users.noreply.github.com>
2023-02-02 19:59:00 +0800

    add test

---
+++ packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-global/array-uniqueBy-dep-case/input.mjs
@@ -0,0 +1,8 @@
+// https://github.com/babel/babel/issues/15392
+
+const data = [
+    { id: 1, uid: 10000 },
+    { id: 2, uid: 10000 },
+    { id: 3, uid: 10001 }
+]
+const final_data = data.uniqueBy(it => it.uid); 
---
+++ packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-global/array-uniqueBy-dep-case/output.mjs
@@ -0,0 +1,14 @@
+import "core-js/modules/es.map.js";
+import "core-js/modules/esnext.array.unique-by.js";
+// https://github.com/babel/babel/issues/15392
+const data = [{
+  id: 1,
+  uid: 10000
+}, {
+  id: 2,
+  uid: 10000
+}, {
+  id: 3,
+  uid: 10001
+}];
+const final_data = data.uniqueBy(it => it.uid);
