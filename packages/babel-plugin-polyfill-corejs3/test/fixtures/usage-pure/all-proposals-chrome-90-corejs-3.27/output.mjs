cd6c626c94ff5e5d35203d863d1e602c0647d22a
Nicolò Ribaudo <hello@nicr.dev>
2023-09-23 16:28:55 +0900

    [core-js] Remove non-existing definitions of iterator methods

--- packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-pure/all-proposals-chrome-90-corejs-3.27/output.mjs
+++ packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-pure/all-proposals-chrome-90-corejs-3.27/output.mjs
@@ -1,12 +1,9 @@
-var _context, _context2, _context3, _context4, _context5, _context6, _context7, _context8, _context9, _context10, _context11, _context12, _context13;
+var _context, _context2, _context3, _context4, _context5, _context6, _context7, _context8, _context9;
 import _findLastInstanceProperty from "core-js-pure/features/instance/find-last.js";
 import _findLastIndexInstanceProperty from "core-js-pure/features/instance/find-last-index.js";
 import _asIndexedPairsInstanceProperty from "core-js-pure/features/instance/asIndexedPairs.js";
-import _toArrayInstanceProperty from "core-js-pure/features/instance/to-array.js";
 import _Iterator$from from "core-js-pure/features/iterator/from.js";
 import _groupByInstanceProperty from "core-js-pure/features/instance/group-by.js";
-import _takeInstanceProperty from "core-js-pure/features/instance/take.js";
-import _dropInstanceProperty from "core-js-pure/features/instance/drop.js";
 import _AsyncIterator$from from "core-js-pure/features/async-iterator/from.js";
 import _uniqueByInstanceProperty from "core-js-pure/features/instance/unique-by.js";
 import _Number$range from "core-js-pure/features/number/range.js";
@@ -21,7 +18,6 @@ import _unThisInstanceProperty from "core-js-pure/features/instance/un-this.js";
 import _pushInstanceProperty from "core-js-pure/features/instance/push.js";
 import _Function$isConstructor from "core-js-pure/features/function/is-constructor.js";
 import _structuredClone from "core-js-pure/features/structured-clone.js";
-import _indexedInstanceProperty from "core-js-pure/features/instance/indexed.js";
 import _withInstanceProperty from "core-js-pure/features/instance/with.js";
 import _isWellFormedInstanceProperty from "core-js-pure/features/instance/is-well-formed.js";
 import _toWellFormedInstanceProperty from "core-js-pure/features/instance/to-well-formed.js";
@@ -33,18 +29,18 @@ import _Iterator from "core-js-pure/features/iterator/index.js";
 _findLastInstanceProperty(_context = []).call(_context, fn);
 _findLastIndexInstanceProperty(_context2 = []).call(_context2, fn);
 _asIndexedPairsInstanceProperty(_context3 = []).call(_context3);
-_toArrayInstanceProperty(_context4 = _Iterator$from({
+_Iterator$from({
   next: () => ({
     done: Math.random() > .9,
     value: Math.random() * 10 | 0
   })
-}).forEach(console.log)).call(_context4);
-_toArrayInstanceProperty(_context5 = _groupByInstanceProperty(_context6 = _takeInstanceProperty(_context7 = _dropInstanceProperty(_context8 = _AsyncIterator$from([1, 2, 3, 4, 5, 6, 7])).call(_context8, 1)).call(_context7, 5).filter(it => it % 2)).call(_context6, it => id % 4).map(it => it ** 2)).call(_context5).then(console.log);
-_groupByInstanceProperty(_context9 = _uniqueByInstanceProperty(_context10 = [1, 2, 3, 4, 5, 6, 7, 7]).call(_context10, x => x)).call(_context9, x => x % 2);
+}).forEach(console.log).toArray();
+_groupByInstanceProperty(_context4 = _AsyncIterator$from([1, 2, 3, 4, 5, 6, 7]).drop(1).take(5).filter(it => it % 2)).call(_context4, it => id % 4).map(it => it ** 2).toArray().then(console.log);
+_groupByInstanceProperty(_context5 = _uniqueByInstanceProperty(_context6 = [1, 2, 3, 4, 5, 6, 7, 7]).call(_context6, x => x)).call(_context5, x => x % 2);
 _Number$range(1, 2);
 _BigInt$range(1n, 2n);
 _Array$isTemplateObject((x => x)`a${x}z`);
-_emplaceInstanceProperty(_context11 = new Map([['x', 1]])).call(_context11, 'x', {
+_emplaceInstanceProperty(_context7 = new Map([['x', 1]])).call(_context7, 'x', {
   update: x => x + 1,
   insert: () => 0
 });
@@ -56,13 +52,13 @@ Symbol.matchAll;
 _Symbol$metadata;
 Symbol.replaceAll;
 new URL(url);
-const foo = _groupByToMapInstanceProperty(_context12 = [1, 2, 3]).call(_context12, bar);
-const push = _unThisInstanceProperty(_context13 = _pushInstanceProperty([])).call(_context13);
+const foo = _groupByToMapInstanceProperty(_context8 = [1, 2, 3]).call(_context8, bar);
+const push = _unThisInstanceProperty(_context9 = _pushInstanceProperty([])).call(_context9);
 _Function$isConstructor;
 throw new DOMException();
 _structuredClone(72);
 btoa('hi');
-_indexedInstanceProperty(foo);
+foo.indexed;
 _withInstanceProperty(bar);
 _pushInstanceProperty(buz);
 fuz.__proto__;
--- packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-pure/all-proposals-corejs-3.20/output.mjs
+++ packages/babel-plugin-polyfill-corejs3/test/fixtures/usage-pure/all-proposals-corejs-3.20/output.mjs
@@ -1,15 +1,12 @@
-var _context, _context2, _context3, _context4, _context5, _context6, _context7, _context8, _context9, _context10, _context11, _context12, _context13, _context14, _context15, _context16;
+var _context, _context2, _context3, _context4, _context5, _context6, _context7, _context8, _context9, _context10, _context11, _context12;
 import _findLastInstanceProperty from "core-js-pure/features/instance/find-last.js";
 import _findLastIndexInstanceProperty from "core-js-pure/features/instance/find-last-index.js";
 import _asIndexedPairsInstanceProperty from "core-js-pure/features/instance/asIndexedPairs.js";
-import _toArrayInstanceProperty from "core-js-pure/features/instance/to-array.js";
 import _forEachInstanceProperty from "core-js-pure/features/instance/for-each.js";
 import _Iterator$from from "core-js-pure/features/iterator/from.js";
 import _mapInstanceProperty from "core-js-pure/features/instance/map.js";
 import _groupByInstanceProperty from "core-js-pure/features/instance/group-by.js";
 import _filterInstanceProperty from "core-js-pure/features/instance/filter.js";
-import _takeInstanceProperty from "core-js-pure/features/instance/take.js";
-import _dropInstanceProperty from "core-js-pure/features/instance/drop.js";
 import _AsyncIterator$from from "core-js-pure/features/async-iterator/from.js";
 import _uniqueByInstanceProperty from "core-js-pure/features/instance/unique-by.js";
 import _Number$range from "core-js-pure/features/number/range.js";
@@ -33,18 +30,18 @@ import _withInstanceProperty from "core-js-pure/features/instance/with.js";
 _findLastInstanceProperty(_context = []).call(_context, fn);
 _findLastIndexInstanceProperty(_context2 = []).call(_context2, fn);
 _asIndexedPairsInstanceProperty(_context3 = []).call(_context3);
-_toArrayInstanceProperty(_context4 = _forEachInstanceProperty(_context5 = _Iterator$from({
+_forEachInstanceProperty(_context4 = _Iterator$from({
   next: () => ({
     done: Math.random() > .9,
     value: Math.random() * 10 | 0
   })
-})).call(_context5, console.log)).call(_context4);
-_toArrayInstanceProperty(_context6 = _mapInstanceProperty(_context7 = _groupByInstanceProperty(_context8 = _filterInstanceProperty(_context9 = _takeInstanceProperty(_context10 = _dropInstanceProperty(_context11 = _AsyncIterator$from([1, 2, 3, 4, 5, 6, 7])).call(_context11, 1)).call(_context10, 5)).call(_context9, it => it % 2)).call(_context8, it => id % 4)).call(_context7, it => it ** 2)).call(_context6).then(console.log);
-_groupByInstanceProperty(_context12 = _uniqueByInstanceProperty(_context13 = [1, 2, 3, 4, 5, 6, 7, 7]).call(_context13, x => x)).call(_context12, x => x % 2);
+})).call(_context4, console.log).toArray();
+_mapInstanceProperty(_context5 = _groupByInstanceProperty(_context6 = _filterInstanceProperty(_context7 = _AsyncIterator$from([1, 2, 3, 4, 5, 6, 7]).drop(1).take(5)).call(_context7, it => it % 2)).call(_context6, it => id % 4)).call(_context5, it => it ** 2).toArray().then(console.log);
+_groupByInstanceProperty(_context8 = _uniqueByInstanceProperty(_context9 = [1, 2, 3, 4, 5, 6, 7, 7]).call(_context9, x => x)).call(_context8, x => x % 2);
 _Number$range(1, 2);
 _BigInt$range(1n, 2n);
 _Array$isTemplateObject((x => x)`a${x}z`);
-_emplaceInstanceProperty(_context14 = new _Map([['x', 1]])).call(_context14, 'x', {
+_emplaceInstanceProperty(_context10 = new _Map([['x', 1]])).call(_context10, 'x', {
   update: x => x + 1,
   insert: () => 0
 });
@@ -56,8 +53,8 @@ _Symbol$matchAll;
 _Symbol$metadata;
 _replaceAllInstanceProperty(_Symbol);
 new _URL(url);
-const foo = _groupByToMapInstanceProperty(_context15 = [1, 2, 3]).call(_context15, bar);
-const push = _unThisInstanceProperty(_context16 = [].push).call(_context16);
+const foo = _groupByToMapInstanceProperty(_context11 = [1, 2, 3]).call(_context11, bar);
+const push = _unThisInstanceProperty(_context12 = [].push).call(_context12);
 _Function$isConstructor;
 throw new _DOMException();
 _structuredClone(72);
