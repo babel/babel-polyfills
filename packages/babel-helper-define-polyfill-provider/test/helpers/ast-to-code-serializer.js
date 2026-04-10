be092d80371391991020809ed6a75e30643aa3de
Nicolò Ribaudo <hello@nicr.dev>
2026-04-10 19:09:12 +0200

    Test Babel 7 compatibility

--- packages/babel-helper-define-polyfill-provider/test/helpers/ast-to-code-serializer.js
+++ packages/babel-helper-define-polyfill-provider/test/helpers/ast-to-code-serializer.js
@@ -1,5 +1,7 @@
 import { types as t } from "@babel/core";
-import generator from "@babel/generator";
+import _generator from "@babel/generator";
+// For compatibility when running tests with Babel 7
+const generator = _generator.default || _generator;
 
 export default {
   test: v => t.isNode(v),
